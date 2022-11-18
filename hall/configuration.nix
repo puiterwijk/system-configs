01d77413614de49d0361392c43c69ded2af00a61
Patrick Uiterwijk <patrick@puiterwijk.org>
2022-11-18 11:27:12 +0100

    Enable flakes
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- hall/configuration.nix
+++ hall/configuration.nix
@@ -40,6 +40,8 @@
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
