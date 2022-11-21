34598242d6017de1c0e8f57c0cb7cd1b86ecb7e7
Patrick Uiterwijk <patrick@puiterwijk.org>
2022-11-21 14:36:53 +0100

    Try rust src
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- hall/users.nix
+++ hall/users.nix
@@ -5,6 +5,7 @@
   ];
   services.vscode-server.enable = true;
 
+  environment.variables.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustcSrc}";
   users.users.puiterwijk = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -17,7 +18,13 @@
       gh
       tmux
       _1password
+
+      cargo
+      rustc
+      rustPlatform.rustcSrc
+      rustPlatform.rustLibSrc
     ];
+
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3wiwWLkUK1rmZnMCN9tVujxOOv44bb6tm7sz/cgZk/ GitHub key"
     ];
