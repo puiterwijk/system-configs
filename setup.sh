0162a90cdccf623fae1aa6440c8b68780501d2b6
Patrick Uiterwijk <patrick@puiterwijk.org>
2023-02-07 10:41:37 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+## CONSTANTS
+GITHUB_URL="https://github.com/puiterwijk/system-configs"
+GIT_URL="${GITHUB_URL}.git"
+RAW_BASE="${GITHUB_URL}/raw/main/"
+
+## CODE STARTS HERE
+if [ $# -ne 1 ];
+then
+    echo "Call: setup.sh <hostname>"
+    exit 1
+fi
+hostname=$1
+
+echo "Setting up for hostname: ${hostname}"
+
+if [ -d /nix/store ];
+then
+    echo "Setting up NixOS system"
+    SYSTEM_TYPE=nixos
+    echo "nixos setup not done, needs redoing"
+    exit 1
+else
+    echo "Setting up a traditional system"
+    SYSTEM_TPYE=traditional
+
+    sudo dnf install -y ansible
+    curl --location --fail "${RAW_BASE}/playbooks/bootstrap.yml" | ansible-playbook /dev/stdin --extra-vars "setup_hostname=${hostname}" --extra-vars "user_home=${HOME}"
+fi
